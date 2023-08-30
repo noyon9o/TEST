11e6d9691763901872aca0acbc9207004c05fff3
noyon <neehadnoyon@gmail.com>
2023-08-30 12:06:18 +0600

    final solution

--- Repetitions.cpp
+++ Repetitions.cpp
@@ -10,41 +10,18 @@ int main()
     string s ;
 
     cin >> s;
-    ll sz = s.size();
-    int A = 0, T = 0, C = 0, G = 0;
-    for (int i = 0; i < sz; i++)
+    int ans = 0, c = 0;
+    char l = 'A';
+    for (char d : s)
     {
-        if(s[i] == 'A')
-            A++;
-        else if (s[i] == 'T')
-            T++;
-        else if (s[i] == 'G')
-            G++;
-        else
-            C++;
+        if(d == l){
+            ++c;
+            ans = max(c , ans);
+        }
+        else{
+            l = d;
+            c = 1;
+        }
     }
-    if(A > T && A > C && A > G)
-        cout << A <<en;
-    else if (T > A && T > C && T > G)
-        cout << T << en;
-    else if (C > A && C > T && C > G)
-        cout << C << en;
-    else if ( A == T && A == C && A == G)
-        cout << 0 << en;
-    else
-        cout << G << en;
-    // cout << sz << en;
-    // cout << s[0]  << en;
-    // cout << s[1]  << en;
-    // cout << s[2]  << en;
-    // cout << s[3]  << en;
-    // cout << s[4]  << en;
-    // cout << s[5]  << en;
-    // cout << s[6]  << en;
-    // cout << s[7]  << en;
-    // cout << s[8]  << en;
-    // cout << s[9]  << en;
-    // cout << s[10]  << en;
-    // cout << s[11]  << en;
-    // cout << s[12]  << en;
+    cout <<ans << en;
 }
