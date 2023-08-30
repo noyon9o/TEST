9f3aab0b7be77827124368ae995c07e6589e4ee8
noyon <neehadnoyon@gmail.com>
2023-08-30 10:54:01 +0600

    Missing nummber

--- Missing_Number.cpp
+++ Missing_Number.cpp
@@ -7,36 +7,12 @@ int main()
     // cout << "Hello world.";
     int n;
     cin >> n;
-    int arr[n];
-    for (int i = 0; i < n-1; i++)
+    int a, sum = 0;
+    for(int i = 1; i <= n; i++)
     {
-        cin >> arr[i];
+        cin >> a;
+        sum += a;   
     }
-    
-    for (int i = 0; i < n-1; i++)
-    {
-        // cout << arr[i] << " ";
-        for (int j = i+1; j < n-1; j++)
-        {
-            if (arr[i] > arr[j])
-            {
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-    }
-    for (int i = 0; i < n-1; i++)
-    {
-        if(arr[i+1] - arr[i] == 2)
-        {
-            cout << arr[i]+1;
-            break;
-        }
-        else
-        {
-            continue;
-        }
-    }
+    cout << sum-n;
     return 0;
 }
