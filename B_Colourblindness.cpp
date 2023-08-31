62d486b6e70902e8680dbfea559a415524d14cda
noyon <neehadnoyon@gmail.com>
2023-08-31 16:47:05 +0600

    commit

--- B_Colourblindness.cpp
+++ B_Colourblindness.cpp
@@ -1,6 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
+const int MAX = 200007;
+const int MOD = 1000000007;
+void splve()
+{
+    int n;
+	cin >> n;
+	string s, t;
+	cin >> s >> t;
+	for (int i = 0; i < n; i++) {
+		if (s[i] == 'R') {
+			if (t[i] != 'R') {cout << "NO\n"; return;}
+		}
+		else {
+			if (t[i] == 'R') {cout << "NO\n"; return;}
+		}
+	}
+	cout << "YES\n";
+}
 int main()
 {
-    cout << "Hello world";
+    ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        splve();
+    }
+    
 }
