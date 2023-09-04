b6bb6bc0ccad51c9633997e5268d51a9d968eda9
noyon <neehadnoyon@gmail.com>
2023-09-04 10:37:50 +0600

    not accepting

--- A_Ambitious_Kid.cpp
+++ A_Ambitious_Kid.cpp
@@ -1,39 +1,36 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
     int n;
     cin >> n;
     int ar[n];
-    int min;
+    int m;
     for (int i = 0; i < n; i++)
     {
         cin >> ar[i];
-        if(ar[i] >= 0 && ar[i] < min)
-            min = ar[i];
+        if (ar[i] >= 0 && ar[i] < m)
+            m = ar[i];
     }
 
     // cout << min;
     for (int i = 0; i < n; i++)
     {
-        if(ar[i] == min)
+        if (ar[i] == m)
         {
-            cout << min;
+            cout << m;
             break;
         }
-        else if (ar[i] < 0 && abs(ar[i]) < min)
+        else if (ar[i] < 0 && abs(ar[i]) < m)
         {
             cout << abs(ar[i]);
             break;
         }
-        
+
         else
         {
             cout << 0;
             break;
         }
-            
     }
-    
-    
 }
