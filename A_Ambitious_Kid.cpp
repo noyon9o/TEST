89f0b340821bd1c98f754b136a36ed2c60cfffef
noyon <neehadnoyon@gmail.com>
2023-09-04 11:06:34 +0600

    commit

--- A_Ambitious_Kid.cpp
+++ A_Ambitious_Kid.cpp
@@ -2,35 +2,45 @@
 using namespace std;
 int main()
 {
-    int n;
+    int n, m = INT_MAX;
     cin >> n;
-    int ar[n];
-    int m;
     for (int i = 0; i < n; i++)
     {
-        cin >> ar[i];
-        if (ar[i] >= 0 && ar[i] < m)
-            m = ar[i];
+        int c;
+        cin >> c;
+        m = min(m, abs(c));
     }
+    cout << m;
 
-    // cout << min;
-    for (int i = 0; i < n; i++)
-    {
-        if (ar[i] == m)
-        {
-            cout << m;
-            break;
-        }
-        else if (ar[i] < 0 && abs(ar[i]) < m)
-        {
-            cout << abs(ar[i]);
-            break;
-        }
+    // int n;
+    // cin >> n;
+    // int ar[n];
+    // int m = INT16_MAX;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cin >> ar[i];
+    //     if (ar[i] >= 0 && ar[i] < m)
+    //         m = ar[i];
+    // }
 
-        else
-        {
-            cout << 0;
-            break;
-        }
-    }
+    // // cout << min;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     if (ar[i] == 0)
+    //     {
+    //         cout << 0;
+    //         break;
+    //     }
+    //     else if (ar[i] < 0 && abs(ar[i]) < m)
+    //     {
+    //         cout << abs(ar[i]);
+    //         break;
+    //     }
+
+    //     else
+    //     {
+    //         cout << m;
+    //         break;
+    //     }
+    // }
 }
