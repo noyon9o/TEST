fdc38918cec02bf512ce30bfd39ddfc975e50522
noyon <neehadnoyon@gmail.com>
2023-09-06 10:24:36 +0600

    ambitious kid

--- A_Ambitious_Kid.cpp
+++ A_Ambitious_Kid.cpp
@@ -1,29 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long int
 int main()
 {
-    int n, m = INT_MAX;
+   
+
+    int n;
     cin >> n;
+    int ar[n];
+    ll m = INT_MAX;
     for (int i = 0; i < n; i++)
     {
-        int c;
-        cin >> c;
-        m = min(m, abs(c));
+        cin >> ar[i];
+        if (abs(ar[i]) < m)
+            m = abs(ar[i]);
     }
+
     cout << m;
-
-    // int n;
-    // cin >> n;
-    // int ar[n];
-    // int m = INT16_MAX;
-    // for (int i = 0; i < n; i++)
-    // {
-    //     cin >> ar[i];
-    //     if (ar[i] >= 0 && ar[i] < m)
-    //         m = ar[i];
-    // }
-
-    // // cout << min;
     // for (int i = 0; i < n; i++)
     // {
     //     if (ar[i] == 0)
@@ -36,7 +29,11 @@ int main()
     //         cout << abs(ar[i]);
     //         break;
     //     }
-
+    //     // else if (ar[i])
+    //     // {
+    //     //     /* code */
+    //     // }
+        
     //     else
     //     {
     //         cout << m;
