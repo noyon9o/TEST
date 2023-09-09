27356b0a8d62d3d50bebe280f5e2bec704dd81d2
noyon <neehadnoyon@gmail.com>
2023-09-09 12:15:19 +0600

    commit c

--- A_Two_Vessels.cpp
+++ A_Two_Vessels.cpp
@@ -1,25 +1,5 @@
-// #include<bits/stdc++.h>
-// using namespace std;
-// int main()
-// {
-//     int t;
-//     cin >> t;
-//     while (t--)
-//     {
-//         int a, b, c;
-//         cin >> a >> b >> c;
-//         int ans = 0;
-//         ans = ((abs(a-b)+1)/2+c-1)/c;
-//         cout << ans << endl;
-//     }
-    
-// }
 #include<bits/stdc++.h>
-
 using namespace std;
-
-#define en endl
-
 int main()
 {
     int t;
@@ -28,30 +8,50 @@ int main()
     {
         int a, b, c;
         cin >> a >> b >> c;
-
-        double re = (a + b) / 2.0;
-        if(a == b)
-            cout << 0 << en;
-        else if (a > b)
-        {
-            double fin = re - b;
-            double ans = fin / c;
-            int answer;
-            if(ans >= 0.1)
-                answer = static_cast<int>(ceil(ans));
-
-             cout << answer << en;
-        }
-        else if (a < b)
-        {
-            double in = re - a;
-            double an = in / c;
-            int nn;
-            if(an >= 0.1)
-                nn = static_cast<int>(ceil(an));
-            cout << nn << en;
-        }
-
+        int ans = 0;
+        ans = ((abs(a-b)+1)/2+c-1)/c;
+        cout << ans << endl;
     }
-    return 0;
+    
 }
+// #include<bits/stdc++.h>
+
+// using namespace std;
+
+// #define en endl
+
+// int main()
+// {
+//     int t;
+//     cin >> t;
+//     while (t--)
+//     {
+//         int a, b, c;
+//         cin >> a >> b >> c;
+
+//         double re = (a + b) / 2.0;
+//         if(a == b)
+//             cout << 0 << en;
+//         else if (a > b)
+//         {
+//             double fin = re - b;
+//             double ans = fin / c;
+//             int answer;
+//             if(ans >= 0.1)
+//                 answer = static_cast<int>(ceil(ans));
+
+//              cout << answer << en;
+//         }
+//         else if (a < b)
+//         {
+//             double in = re - a;
+//             double an = in / c;
+//             int nn;
+//             if(an >= 0.1)
+//                 nn = static_cast<int>(ceil(an));
+//             cout << nn << en;
+//         }
+
+//     }
+//     return 0;
+// }
