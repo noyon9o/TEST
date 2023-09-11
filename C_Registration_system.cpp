8b5ff291b7815d76a7d64619ddf6e04e886bbea7
noyon <neehadnoyon@gmail.com>
2023-09-11 12:53:41 +0600

    commit m

--- C_Registration_system.cpp
+++ C_Registration_system.cpp
@@ -2,8 +2,32 @@
 
 using namespace std;
 
+void solve()
+{
+    int n;
+    //cin >> n;
+    map<int, string> mp;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     string s;
+    //     cin >> s;
+    // }
+    
+
+    
+}
 
 int main()
 {
+    solve();
 
+    map<int, string> sample_map;
+
+  sample_map.insert(pair<int, string>(1, "one"));
+
+  sample_map.insert(pair<int, string>(2, "two"));
+
+  cout << sample_map[1] << " " << sample_map[2] << endl;
+
+  return false;
 }
