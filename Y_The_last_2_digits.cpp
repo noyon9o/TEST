6b90714542295fc8579f5ecf99c10adea0004360
noyon <neehadnoyon@gmail.com>
2023-09-25 12:36:18 +0600

    merge

---
+++ Y_The_last_2_digits.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void solve()
+{
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    a = a%100;
+    b = b % 100;
+    c = c %100;
+    d = d % 100;
+
+    int x = a*b*c*d;
+    x = x%100;
+
+    if(x < 10)
+        cout  << "0" << x;
+    else
+        cout << x;
+}
+
+int main(void)
+{
+    solve();
+
+
+    return false;
+}
